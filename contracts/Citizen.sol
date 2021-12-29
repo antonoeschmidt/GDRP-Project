758acd6f2c3e31a86f07a48742f9c4254b5fddfb
HaraldBruun <s185014@student.dtu.dk>
2021-12-29 10:47:56 +0100

    contract

--- contracts/Citizen.sol
+++ contracts/Citizen.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.10;
+pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2; //only for testing
 
 contract Citizen {
@@ -10,7 +10,8 @@ contract Citizen {
     // mapping(string => string) private data;
     // the dataIds could be encrypted from the backEnd if necessary (symmertic /AES or asymmertic RSA)
     // or just an array
-    string[] private dataIds; 
+    // string[] private dataIds; 
+    mapping(string => string) private data;
     
     // this hashMap will hold dataId as Key, and a list of walletIds (or Company IDs), that have permission to that
     // specific data when permissions change, the walletId will either be added or removed from that specific dataId / entry
@@ -18,24 +19,25 @@ contract Citizen {
     
     address owner;
 
-    constructor() {
+    constructor() public {
         owner = msg.sender;
     }  
     
     // TODO: implement this. 
-    // function addData (string dataId) {
-    //      require(msg.sender == owner);
-    //      data.append(dataId)
-    //      permissions[dataId] = new Array();
-    // }
+    function addData (string memory dataId, string memory hash) public {
+         require(msg.sender == owner);
+         address[] memory array;
+         data[dataId] = hash; 
+         permissions[dataId] = array;
+    }
 
     // TODO: implement this. 
-    // function givePermission (companyId / wallet, dataId) {
-    //      require(msg.sender == owner);
-    //      list = permissions[dataId];
-    //      list.append(companyId);
-    //      permissions[dataId] = list;
-    // }
+    function givePermission (address requester, string memory dataId) public {
+         require(msg.sender == owner);
+         address[] storage list = permissions[dataId];
+         list.push(requester);
+         permissions[dataId] = list;
+    }
 
     // TODO: implement this. 
     // function revokePermission (companyId / wallet, dataId) {
