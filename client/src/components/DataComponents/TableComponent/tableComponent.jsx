2448f3c92f61758a0880537422303249ccfc664c
HaraldBruun <s185014@student.dtu.dk>
2021-12-30 14:42:21 +0100

    removed row comp

--- client/src/components/DataComponents/TableComponent/tableComponent.jsx
+++ client/src/components/DataComponents/TableComponent/tableComponent.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
-import { Table } from "semantic-ui-react";
-import RowComponent from "../RowComponent/rowComponent";
+import { Table, Button } from "semantic-ui-react";
 
 const TableComponent = ({ props }) => {
   const id = localStorage.getItem("id");
@@ -22,6 +21,22 @@ const TableComponent = ({ props }) => {
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const sendDeleteData = (id) => {
+    return fetch(`http://localhost:3001/data/dataid/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    }).then((res, err) => {
+      if (res.status === 200){
+        props.setUserData(props.userData.filter((d) => d._id !== id));
+      } else {
+        console.log(`Delete failed : ${err}`)
+      }
+    });
+  };
+
   return (
     <Table striped>
       <Table.Header>
@@ -34,7 +49,19 @@ const TableComponent = ({ props }) => {
       <Table.Body>
         {props.userData &&
           props.userData.map((d) => (
-            <RowComponent props={d} key={d._id}></RowComponent>
+            <Table.Row key={d._id}>
+            <Table.Cell>{d._id}</Table.Cell>
+            <Table.Cell>{d.content}</Table.Cell>
+            <Table.Cell>
+              <Button
+                onClick={() => {
+                  sendDeleteData(d._id);
+                }}
+              >
+                Delete
+              </Button>
+            </Table.Cell>
+          </Table.Row>
           ))}
       </Table.Body>
     </Table>
