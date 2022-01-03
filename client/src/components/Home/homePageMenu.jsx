df7922b422e3acebbf7dd67ee6242c0287371504
HaraldBruun <s185014@student.dtu.dk>
2022-01-03 22:10:11 +0100

    request menu

--- client/src/components/Home/homePageMenu.jsx
+++ client/src/components/Home/homePageMenu.jsx
@@ -33,6 +33,7 @@ const HomePageMenu = ({activeMenu}) => {
                 )}
                 {loggedIn && <Menu.Item as="a" onClick={() => navigate("/data")} active={activeMenu === "data"}>
                     Data</Menu.Item>}
+                {loggedIn && <Menu.Item as="a" onClick={() => navigate("/requests")} active={activeMenu === "requests"}>Requests</Menu.Item>}
                 {loggedIn && <Menu.Item as="a" onClick={() => navigate("/permissions")} active={activeMenu === "permissions"}>Permissions</Menu.Item>}
                 <Menu.Item as="a">Credits</Menu.Item>
                 <Menu.Item position="right">
